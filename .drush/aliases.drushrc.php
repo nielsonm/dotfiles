d2b0f23fd9c8c432b0ef0656e0bbb384b6c31d28
Mike Nielson <michael@opensourcery.com>
2014-03-07 08:51:24 -0800

    Updated drush aliases.

--- .drush/aliases.drushrc.php
+++ .drush/aliases.drushrc.php
@@ -38,35 +38,61 @@ $aliases['tscf-oa-prod'] = array(
   'user' => 'tscfdire',
   'root' => '/home/tscfdire/public_html'
 );
+$aliases['tek'] = array(
+  'uri' => 'tek.com',
+  'root' => '/home/michael/work/client/tek/app/pressflow/sites/tek.com',
+  'user' => 'admin'
+);
+$aliases['psu.staging'] = array(
+  'root' => '/var/www/current',
+  'uri' => 'http://cota-staging.opensourcery.com',
+  'remote-user' => 'jhedstrom',
+  'remote-host' => 'cota-staging.opensourcery.com',
+  'ssh-options' => '-p 2276',
+);
 
 
 // Gargravarr test hosts.
 $gargravarr = array(
+  'adams',
   'atka',
   'bachelor',
+  'belknap',
+  'capulin',
   'copahue',
+  'cota-staging',
   'demo',
   'dempo',
-  'dempo',
+  'dotsero',
+  'ekarma',
   'eldfell',
   'fuego',
   'galeras',
+  'gamalama',
   'hood',
   'isspah',
+  'izalco',
   'jornada',
   'karthala',
   'langila',
+  'lopevi',
   'myoko',
   'nazko',
+  'newberry',
   'okataina',
+  'opala',
   'pinatubo',
   'qualibou',
   'rainier',
+  'regionalprograms',
   'shasta',
+  'sotara',
+  'trocon',
   'tsurumi',
   'undara',
   'visoke',
   'waiowa',
+  'wurlali',
   'yellowstone',
   'zimina',
 );
@@ -74,15 +100,54 @@ foreach ($gargravarr as $g_host) {
   $aliases[$g_host] = array(
     'uri' => 'default',
     'root' => '/var/www/' . $g_host . '/current',
-    'remote-user' => $_SERVER['USER'],
-    'remote-host' => 'gargravarr.opensourcery.com',
+    'remote-host' => 'gargravarr',
   );
 }
 
-$aliases['dempo-catalog'] = array(
-  'uri' => 'default',
-  'root' => '/var/www/dempo/current/catalog',
-  'remote-user' => $_SERVER['USER'],
-  'remote-host' => 'gargravarr.opensourcery.com',
+$aliases['ilfi.test'] = array(
+  'root' => '.',
+  'uri' => 'test.living-future.org',
+  'db-url' => 'mysql://pantheon:ede7c35763be4b77a718f1cae4def114@dbserver.test.a6298332-55bd-4ad2-a984-782f0ab3cfdc.drush.in:10821/pantheon',
+  'db-allows-remote' => TRUE,
+  'remote-host' => 'appserver.test.a6298332-55bd-4ad2-a984-782f0ab3cfdc.drush.in',
+  'remote-user' => 'test.a6298332-55bd-4ad2-a984-782f0ab3cfdc',
+  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
+  'path-aliases' => array(
+    '%files' => 'code/sites/default/files'
+  ),
+);
+$aliases['ilfi.live'] = array(
+  'root' => '.',
+  'uri' => 'www.living-future.org',
+  'db-url' => 'mysql://pantheon:1c2fadf24ac94f848bcf4b477da80c5c@dbserver.live.a6298332-55bd-4ad2-a984-782f0ab3cfdc.drush.in:10046/pantheon',
+  'db-allows-remote' => TRUE,
+  'remote-host' => 'appserver.live.a6298332-55bd-4ad2-a984-782f0ab3cfdc.drush.in',
+  'remote-user' => 'live.a6298332-55bd-4ad2-a984-782f0ab3cfdc',
+  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
+  'path-aliases' => array(
+    '%files' => 'code/sites/default/files'
+  ),
+);
+$aliases['ilfi.dev'] = array(
+  'root' => '.',
+  'uri' => 'dev.ilfi.gotpantheon.com',
+  'db-url' => 'mysql://pantheon:bf74f9353f7d400dab79722c86f91427@dbserver.dev.a6298332-55bd-4ad2-a984-782f0ab3cfdc.drush.in:11637/pantheon',
+  'db-allows-remote' => TRUE,
+  'remote-host' => 'appserver.dev.a6298332-55bd-4ad2-a984-782f0ab3cfdc.drush.in',
+  'remote-user' => 'dev.a6298332-55bd-4ad2-a984-782f0ab3cfdc',
+  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
+  'path-aliases' => array(
+    '%files' => 'code/sites/default/files'
+  ),
 );
 
+$directories = glob('/var/www/dev/*');
+
+foreach ($directories as $directory) {
+  $name = array_pop(explode('/', $directory));
+
+  $aliases[$name] = array(
+    'uri' => "http://$name.dev",
+    'root' => $directory,
+  );
+}
