698afe12ed4a215b3ad2ac5908f25c8f21fdefa8
Mike Nielson <michael@opensourcery.com>
2014-08-08 11:28:20 -0700

    Changed git parsing to work on 1.7 and bash.

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -19,14 +19,18 @@ local PROMPTNAME="${GREEN}\\u${BLUE}@${CYAN}\\h${CRESET} "
 
 ref=$(git symbolic-ref HEAD 2> /dev/null)
 if [[ -n $ref ]]; then
-  gitstat=$(git status 2>/dev/null | grep '^\(Untracked\|Changes\|Changed but not updated:\)')
-  if [[ $(echo ${gitstat} | grep -c "^\(Changes to be committed:\|Changes not staged for commit:\)$") > 0 ]]; then
-    local gitbang='!'
+  # TODO remove the '# ' after getting to git 1.8
+  gitstat=$(git status 2>/dev/null | grep '^\(# Untracked\|# Changes\|# Changed but not updated:\)')
+  if [[ $(echo "${gitstat}" | grep -c "^\(# Changes not staged for commit:\)$") > 0 ]]; then
+    local gitbang='*'
   fi
-  if [[ $(echo ${gitstat} | grep -c "^\(Untracked files:\|Changed but not updated:\)$") > 0 ]]; then
+  if [[ $(echo "${gitstat}" | grep -c "^\(# Changes to be committed:\)$") > 0 ]]; then
+    local gitcross='+'
+  fi
+  if [[ $(echo "${gitstat}" | grep -c "^\(# Untracked files:\|Changed but not updated:\)$") > 0 ]]; then
     local gitqmark='?'
   fi
-  local PROMPTBRANCH="${BLUE}on$DARK_GRAY$(__git_ps1)$CYAN$gitbang$gitqmark$CRESET "
+  local PROMPTBRANCH="${BLUE}on $DARK_GRAY${ref#refs/heads/}$CYAN$gitbang$gitcross$gitqmark$CRESET "
 fi
 
 f="${TMPDIR:-/tmp/}/drush-env/drush-drupal-site-$$"
